ac77b1cef8528e97f4cd30158de720d0a9621073
Apurva Hingul <apurvahingul123@gmail.com>
2021-11-19 14:45:30 +0000

    Seperated the subjects data to subjects.js

--- subjects.js
+++ subjects.js
@@ -2,121 +2,111 @@ let NoOfSubjects = [
 
     {
         id: 1001,
-        title: "Math" ,
+        subject: "Math" ,
         location: "Centrel London" ,
         price: 100,
         image: "image/math.jpg", 
         imageWidth: 200, //expanding the image
         availableInventory: 5
-        rating : 2,
         
     },
     
     {
         id: 1002,
-        title: "English" ,
+        subject: "English" ,
         location: "Scotland" ,
         price: 200,
         image: "image/English.png", 
         imageWidth: 200, //expanding the image
         availableInventory: 5
-        rating : 3,
         
     },
     
     {
         id: 1003,
-        title: "Music" ,
+        subject: "Music" ,
         location: "Manchester" ,
         price: 100,
         image: "image/Music.jpg", 
         imageWidth: 200, //expanding the image
         availableInventory: 5
-        rating : 5,
         
     },
     
     {
         id: 1004,
-        title: "Geography" ,
+        subject: "Geography" ,
         location: "Liverpool" ,
         price: 50,
         image: "image/Geography.jpg", 
         imageWidth: 200, //expanding the image
         availableInventory: 5
-        rating : 3,
         
     },
     
     {
         id: 1005,
-        title: "History" ,
+        subject: "History" ,
         location: "Surrey" ,
         price: 70,
         image: "image/History.jpg", 
         imageWidth: 200, //expanding the image
         availableInventory: 5
-        rating : 3,
         
     },
     
     {
         id: 1006,
-        title: "Fine Art" ,
+        subject: "Fine Art" ,
         location: "Bristol" ,
         price: 90,
         image: "image/Art.jpg", 
         imageWidth: 200, //expanding the image
         availableInventory: 5
-        rating : 5,
         
     },
     
     {
         id: 1007,
-        title: "Social Studies" ,
+        subject: "Social Studies" ,
         location: "Essex" ,
         price: 75,
         image: "image/Social.jpg", 
         imageWidth: 200, //expanding the image
         availableInventory: 5
-        rating : 5,
         
     },
     
     {
         id: 1008,
-        title: "Science" ,
+        subject: "Science" ,
         location: "Norwich" ,
         price: 100,
         image: "image/Science.jpg", 
         imageWidth: 200, //expanding the image
         availableInventory: 5
-        rating : 5,
         
     },
     
     {
         id: 1009,
-        title: "Business Management",
+        subject: "Business Management",
         location: "Newcastle" ,
         price: 100,
         image: "image/Business.jpg", 
         imageWidth: 200, //expanding the image
         availableInventory: 5
-        rating : 5,
         
     },
     
     {
         id: 1010,
-        title: "IT",
+        subject: "IT",
         location: "Kent" ,
         price: 100,
         image: "image/IT.jpg", 
         imageWidth: 200, //expanding the image
         availableInventory: 5
-        rating : 5,
         
     },
     ];
