8368c7ca25ba53c885bbd0a9b2c66d2404776015
Apurva Hingul <apurvahingul123@gmail.com>
2021-11-19 04:01:46 +0000

    Added all class data and images

---
+++ subjects.js
@@ -0,0 +1,122 @@
+let NoOfLessons = [
+
+    {
+        id: 1001,
+        title: "Math" ,
+        location: "Centrel London" ,
+        price: 100,
+        image: "image/math.jpg", 
+        imageWidth: 200, //expanding the image
+        availableInventory: 5
+        rating : 2,
+        
+    },
+    
+    {
+        id: 1002,
+        title: "English" ,
+        location: "Scotland" ,
+        price: 200,
+        image: "image/English.png", 
+        imageWidth: 200, //expanding the image
+        availableInventory: 5
+        rating : 3,
+        
+    },
+    
+    {
+        id: 1003,
+        title: "Music" ,
+        location: "Manchester" ,
+        price: 100,
+        image: "image/Music.jfif", 
+        imageWidth: 200, //expanding the image
+        availableInventory: 5
+        rating : 5,
+        
+    },
+    
+    {
+        id: 1004,
+        title: "Geography" ,
+        location: "Liverpool" ,
+        price: 50,
+        image: "image/Geography.jpg", 
+        imageWidth: 200, //expanding the image
+        availableInventory: 5
+        rating : 3,
+        
+    },
+    
+    {
+        id: 1005,
+        title: "History" ,
+        location: "Surrey" ,
+        price: 70,
+        image: "image/History.jpg", 
+        imageWidth: 200, //expanding the image
+        availableInventory: 5
+        rating : 3,
+        
+    },
+    
+    {
+        id: 1006,
+        title: "Fine Art" ,
+        location: "Bristol" ,
+        price: 90,
+        image: "image/Art.jpg", 
+        imageWidth: 200, //expanding the image
+        availableInventory: 5
+        rating : 5,
+        
+    },
+    
+    {
+        id: 1007,
+        title: "Social Studies" ,
+        location: "Essex" ,
+        price: 75,
+        image: "image/Social.jpg", 
+        imageWidth: 200, //expanding the image
+        availableInventory: 5
+        rating : 5,
+        
+    },
+    
+    {
+        id: 1008,
+        title: "Science" ,
+        location: "Norwich" ,
+        price: 100,
+        image: "image/Science.jpg", 
+        imageWidth: 200, //expanding the image
+        availableInventory: 5
+        rating : 5,
+        
+    },
+    
+    {
+        id: 1009,
+        title: "Business Management",
+        location: "Newcastle" ,
+        price: 100,
+        image: "image/Business.jpg", 
+        imageWidth: 200, //expanding the image
+        availableInventory: 5
+        rating : 5,
+        
+    },
+    
+    {
+        id: 1010,
+        title: "IT",
+        location: "Kent" ,
+        price: 100,
+        image: "image/IT.jpg", 
+        imageWidth: 200, //expanding the image
+        availableInventory: 5
+        rating : 5,
+        
+    },
+    ];
